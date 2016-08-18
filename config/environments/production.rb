697071049177e68c656dd20446f3d7121b2a00c9
Tien Vu <tvucpa@gmail.com>
2016-08-18 19:00:07 -0400

    email set for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,21 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # email enabled in production
+ config.action_mailer.smtp_settings = {
+   address: "smtp.sendgrid.net",
+   port: 587,
+   domain: Rails.application.secrets.domain_name,
+   authentication: "plain",
+   user_name: Rails.application.secrets.email_provider_username,
+   password: Rails.application.secrets.email_provider_password
+ }
+ # ActionMailer Config
+ config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.perform_deliveries = true
+ config.action_mailer.raise_delivery_errors = false
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
